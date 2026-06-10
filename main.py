4f8762d293553f6dc6d718269aff63eec65e6595
Jason Nguyen <jason.vbnguyen@gmail.com>
2026-06-10 16:15:50 -0500

    added D term- dampens oscillations

--- main.py
+++ main.py
@@ -24,18 +24,28 @@ def throttle2thrust(throttle):
 
 clamp = lambda x,mn,mx: max(mn, min(mx,x))
 class Controller:
-    def __init__(self):
-        self.kp = 2
-        self.ki = 0.0
+    def __init__(self, kp=2, ki=0, kd=0):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
         self.cumul_err = 0
+        self.last_err = None
     
     def run(self, target, current):
         '''returns throttle %'''
         err = target-current #positive if lower
         self.cumul_err += TIME_STEP*err
-        self.cumul_err = clamp(self.cumul_err, -50,50)
+        self.cumul_err = clamp(self.cumul_err, -100,100)
 
-        return clamp(self.kp*err + self.ki*self.cumul_err, 0, 100)
+        d_err = 0
+        if self.last_err is not None:
+            d_err = (err-self.last_err)/TIME_STEP
+        self.last_err = err
+
+        return clamp(self.kp*err 
+            + self.ki*self.cumul_err
+            + self.kd*d_err, 
+            0, 100)
 
 t_vals=[]
 alt_hist=[]
@@ -48,9 +58,8 @@ def simulate(controller):
     throttle_hist.clear()
     cumulerr_hist.clear()
 
-    con = Controller()
-    con.kp = controller.kp
-    con.ki = controller.ki
+    con = Controller(
+        controller.kp, controller.ki, controller.kd)
     t = 0
     alt = 0
     vel = 0
@@ -79,6 +88,7 @@ def draw(controller):
     fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=4, ncols=1, 
                                         sharex=True,
                                         figsize=(9,9))
+    
     l1, = ax1.plot(t_vals, alt_hist)
     ax1.set_title('altitude')
     l2, = ax2.plot(t_vals, vel_hist)
@@ -88,20 +98,25 @@ def draw(controller):
     l4, = ax4.plot(t_vals, cumulerr_hist)
     ax4.set_title('cumul_err')
 
+    ax1.plot(t_vals, [TARGET]*len(t_vals)) #target line
+
     plt.subplots_adjust(left=0.1, right=0.9, bottom=0.2, top=0.95)
 
     # sliders
     from matplotlib.widgets import Slider
 
     ax_kp = plt.axes([0.1, 0.12, 0.7, 0.03]) #left,bottom,width,height
-    ax_ki = plt.axes([0.1, 0.06, 0.7, 0.03])
+    ax_ki = plt.axes([0.1, 0.08, 0.7, 0.03])
+    ax_kd = plt.axes([0.1, 0.04, 0.7, 0.03])
 
     s_kp = Slider(ax_kp, 'kp', 0.0, 10.0, valinit=controller.kp)
     s_ki = Slider(ax_ki, 'ki', 0, 1.0, valinit=controller.ki)
+    s_kd = Slider(ax_kd, 'kd', 0.0, 5.0, valinit=controller.kd)
 
     def _update(val):
         controller.kp = s_kp.val
         controller.ki = s_ki.val
+        controller.kd = s_kd.val
         simulate(controller)
         l1.set_ydata(alt_hist)
         l2.set_ydata(vel_hist)
@@ -120,6 +135,7 @@ def draw(controller):
 
     s_kp.on_changed(_update)
     s_ki.on_changed(_update)
+    s_kd.on_changed(_update)
 
     plt.show()
 
